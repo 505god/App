522bf38d1e614f42fce7362b36e807806694ce15
邱成西 <qiuchengxi@juhuacan.local>
2015-02-02 08:23:48 +0800

    Initial Commit

---
+++ App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  App
+//
+//  Created by 邱成西 on 15/2/2.
+//  Copyright (c) 2015年 Just Do It. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  App
+//
+//  Created by 邱成西 on 15/2/2.
+//  Copyright (c) 2015年 Just Do It. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
